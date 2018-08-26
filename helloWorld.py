b6e8f3709bb59eb302e0c4b4d822c541a6aebb3d
Eric Nordberg <enordberg@gmail.com>
2018-08-25 22:03:43 -0500

    more testing

--- helloWorld.py
+++ helloWorld.py
@@ -1,3 +1,7 @@
 msg = "Hello World"
-print(msg)
-
+msg2 = "\n\tThis is boring..."
+print(msg.upper() + msg2.lstrip())
+num1= 2
+num2 = 3
+print(num1 + num2)
+print(str(num1) + str(num2)) # should be 23
