2fc1858acd6fd4864853543a5cbcf1f2588221ff
Eric Nordberg <enordberg@gmail.com>
2018-09-03 21:31:22 -0500

    finish prime

--- prime.py
+++ prime.py
@@ -1,27 +1,30 @@
+def is_prime(number):
+    "Returns true if the number is prime."
+    if (number < 2):
+        return False
+    elif (number < 4):
+        return True
+    for value in range(2, number):
+        if (number!=value) and (number % value == 0):
+            return False
+    return True
+
 highestNumberInput = input("How high should we go?  ")
 highestNumber = int(highestNumberInput)
 if (highestNumber<1) or (highestNumber>100):
    print("The hightest number must be between 1 and 100.")
-
-# tod0 - keep editing from here
-sum = 1
-previousNumber = 0
-tempNumber = 0
-currentIteration = 0
-values = []
-while currentIteration<numberOfIterations:
-    tempNumber = sum
-    sum = sum + previousNumber
-    previousNumber = tempNumber
-    currentIteration += 1
-    values.append(sum)
+primes = []
+for value in range(1, highestNumber):
+    if (is_prime(value)):
+        primes.append(value)
 isFirst = True
-result = ""
-for value in values:
+results = ""
+for value in primes:
     if (not isFirst):
-        result = result + ", "
+        results = results + ", " + str(value)
     else:
+        results = str(value)
         isFirst = False
-    result = result + str(value)
-print(result)
+print(results)
+
 
